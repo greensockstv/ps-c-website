72cd1feaadf9b41e3488eb6b2098963e1d82add6
Felix <felixm.tatschl@gmx.at>
2020-09-18 16:17:21 +0200

    start

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
 import React from "react"
-
+import styles from "../styles/Index.module.css"
+import { AppBar, Toolbar, IconButton, Grid } from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Grid container alignItems="center" justify="space-between">
+            <Grid item>
+              <div>
+                <img src="static/schriftzugPSk2.jpg" className={styles.logo} />
+              </div>
+            </Grid>
+            <Grid item>
+              <IconButton edge="start" color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
 }
